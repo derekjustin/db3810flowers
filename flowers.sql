40ef50cd82df3b4d3a690ea829bead1c57432dd0
Justin Strelka <jstrelka@student.cccs.edu>
2019-09-19 12:15:17 -0600

    Finished defining FlowersInfo table

--- flowers.sql
+++ flowers.sql
@@ -9,20 +9,26 @@ USE flowers;
 --Create tables
 
 CREATE TABLE Zones(
-    id INTEGER NOT NULL PRIMARY AUTO_INCREMENT,
-    lowerTemp INTEGER NOT NULL,
-    higerTemp INTEGER NOT NULL )
+    id TINYINT(2) NOT NULL PRIMARY AUTO_INCREMENT,
+    lowerTemp TINYINT(2) NOT NULL,
+    higerTemp TINYINT(2) NOT NULL )
     AUTO_INCREMENT = 2;
 
 CREATE TABLE Deliveries(
-    id INTEGER NOT NULL PRIMARY AUTO_INCREMENT,
-    categ VARCHAR(15) NOT NULL,
+    id TINYINT(1) NOT NULL PRIMARY AUTO_INCREMENT,
+    categ VARCHAR(5) NOT NULL,
     delSize DECIMAL(5,3)
-)
+);
 
-FLowersInfo(
-
-)
+CREATE TABLE FLowersInfo(
+    id CHAR(3) NOT NULL PRIMARY AUTO_INCREMENT,
+    comName VARCHAR(30) NOT NULL,
+    latName VARCHAR(35) NOT NULL,
+    cZone TINYINT(2) NOT NULL,
+    hZone TINYINT(2) NOT NULL,
+    deliver TINYINT(2) NOT NULL,
+    sunNeeds VARCHAR(5)
+);
 
 --Populate tables
 
