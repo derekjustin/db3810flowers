273dc49023a30f5a9433d1f4833fc641fedec596
Justin Strelka <jstrelka@student.cccs.edu>
2019-09-19 11:46:03 -0600

    Created flowers.sql file: created db, created zones and Deliveries tables.

---
+++ flowers.sql
@@ -0,0 +1,29 @@
+--Create database
+
+CREATE DATABASE flowers;
+
+--Select Database
+
+USE flowers;
+
+--Create tables
+
+CREATE TABLE Zones(
+    id INTEGER NOT NULL PRIMARY AUTO_INCREMENT,
+    lowerTemp INTEGER NOT NULL,
+    higerTemp INTEGER NOT NULL )
+    AUTO_INCREMENT = 2;
+
+CREATE TABLE Deliveries(
+    id INTEGER NOT NULL PRIMARY AUTO_INCREMENT,
+    categ VARCHAR(15) NOT NULL,
+    delSize DECIMAL(5,3)
+)
+
+FLowersInfo(
+
+)
+
+--Populate tables
+
+--Query db
