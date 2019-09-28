0ae9c662f8ae8cc4f9db2e3d06bc0c72005500c1
Justin Strelka <jstrelka@student.cccs.edu>
2019-09-28 10:54:04 -0600

    updated flowerinfo table not to autoincrement ids

--- flowers.sql
+++ flowers.sql
@@ -21,7 +21,7 @@ CREATE TABLE Deliveries(
 ) AUTO_INCREMENT = 1;
 
 CREATE TABLE FlowersInfo(
-    id TINYINT(3) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id TINYINT(3) NOT NULL PRIMARY KEY,
     comName VARCHAR(30) NOT NULL,
     latName VARCHAR(35) NOT NULL,
     cZone TINYINT(2) NOT NULL,
@@ -33,7 +33,7 @@ CREATE TABLE FlowersInfo(
     FOREIGN KEY (hZone) REFERENCES Zones (id),
     FOREIGN KEY (deliver) REFERENCES Deliveries (id)
 
-) AUTO_INCREMENT = 101;
+);
 
 
 -- Populate tables
@@ -61,16 +61,16 @@ INSERT INTO Deliveries(categ,delSize) VALUES ('shrub',24.000);
 INSERT INTO Deliveries(categ,delSize) VALUES ('tree',36.000);
 
 -- flowersInfo
-INSERT INTO FlowersInfo(comName,latName,cZone,hZone,deliver,sunNeeds) VALUES ('Lady Fern','Atbyrium filix-femina',2,9,5,'SH');
-INSERT INTO FlowersInfo(comName,latName,cZone,hZone,deliver,sunNeeds) VALUES ('Pink Caladiums','C.x bortulanum',10,10,6,'PtoSH');
-INSERT INTO FlowersInfo(comName,latName,cZone,hZone,deliver,sunNeeds) VALUES ('Lily-of-the-Valley','Convallaria majalis',2,8,5,'PtoSH');
-INSERT INTO FlowersInfo(comName,latName,cZone,hZone,deliver,sunNeeds) VALUES ('Purple Liatris','Liatris spicata',3,9,6,'StoP');
-INSERT INTO FlowersInfo(comName,latName,cZone,hZone,deliver,sunNeeds) VALUES ('Black Eyed Susan','Rudbeckia fulgida var. specios',4,10,2,'StoP');
-INSERT INTO FlowersInfo(comName,latName,cZone,hZone,deliver,sunNeeds) VALUES ('Nikko Blue Hydrangea','Hydrangea macrophylla',5,9,4,'StoSH');
-INSERT INTO FlowersInfo(comName,latName,cZone,hZone,deliver,sunNeeds) VALUES ('Variegated Weigela','W. florida Variegata',4,9,8,'StoP');
-INSERT INTO FlowersInfo(comName,latName,cZone,hZone,deliver,sunNeeds) VALUES ('Lombardy Poplar','Populus nigra Italica',3,9,9,'S');
-INSERT INTO FlowersInfo(comName,latName,cZone,hZone,deliver,sunNeeds) VALUES ('Purple Leaf Plum Hedge','Prunus x cistena',2,8,7,'S');
-INSERT INTO FlowersInfo(comName,latName,cZone,hZone,deliver,sunNeeds) VALUES ('Thorndale Ivy','Hedera belix Thorndale',3,9,1,'StoSH');
+INSERT INTO FlowersInfo(id,comName,latName,cZone,hZone,deliver,sunNeeds) VALUES (101,'Lady Fern','Atbyrium filix-femina',2,9,5,'SH');
+INSERT INTO FlowersInfo(id,comName,latName,cZone,hZone,deliver,sunNeeds) VALUES (102,'Pink Caladiums','C.x bortulanum',10,10,6,'PtoSH');
+INSERT INTO FlowersInfo(id,comName,latName,cZone,hZone,deliver,sunNeeds) VALUES (103,'Lily-of-the-Valley','Convallaria majalis',2,8,5,'PtoSH');
+INSERT INTO FlowersInfo(id,comName,latName,cZone,hZone,deliver,sunNeeds) VALUES (105,'Purple Liatris','Liatris spicata',3,9,6,'StoP');
+INSERT INTO FlowersInfo(id,comName,latName,cZone,hZone,deliver,sunNeeds) VALUES (106,'Black Eyed Susan','Rudbeckia fulgida var. specios',4,10,2,'StoP');
+INSERT INTO FlowersInfo(id,comName,latName,cZone,hZone,deliver,sunNeeds) VALUES (107,'Nikko Blue Hydrangea','Hydrangea macrophylla',5,9,4,'StoSH');
+INSERT INTO FlowersInfo(id,comName,latName,cZone,hZone,deliver,sunNeeds) VALUES (108,'Variegated Weigela','W. florida Variegata',4,9,8,'StoP');
+INSERT INTO FlowersInfo(id,comName,latName,cZone,hZone,deliver,sunNeeds) VALUES (110,'Lombardy Poplar','Populus nigra Italica',3,9,9,'S');
+INSERT INTO FlowersInfo(id,comName,latName,cZone,hZone,deliver,sunNeeds) VALUES (111,'Purple Leaf Plum Hedge','Prunus x cistena',2,8,7,'S');
+INSERT INTO FlowersInfo(id,comName,latName,cZone,hZone,deliver,sunNeeds) VALUES (114,'Thorndale Ivy','Hedera belix Thorndale',3,9,1,'StoSH');
 
 -- TODO: Query db
 -- a) the total number of zones.
@@ -80,6 +80,7 @@ SELECT COUNT(*) AS 'Total Zones' FROM zones;
 
 
 -- c) common names of the plants that have delivery sizes less than 5.
+
 -- d) common names of the plants that require full sun (i.e., sun needs contains ‘S’).
 -- e) all delivery category names order alphabetically (without repetition).
 -- f) the exact output (note that it is order by Name):
