f07aa601df19559d4f6311748a68f287e0d597d0
Justin Strelka <jstrelka@student.cccs.edu>
2019-09-28 13:52:35 -0600

    added the rest of the queries to the list

--- flowers.sql
+++ flowers.sql
@@ -90,3 +90,15 @@ SELECT categ AS 'Delivery Category Names' FROM Deliveries GROUP BY categ ORDER B
 
 -- f) the exact output (note that it is order by Name):
 SELECT A.comName AS 'Name',B.lowerTemp AS 'Cool Zone (low)',B.higherTemp AS 'Cool Zone (high)',C.categ AS 'Delivery Category' FROM FlowersInfo A INNER JOIN Zones B ON A.cZone = B.id,Deliveries C WHERE C.id = A.deliver ORDER BY A.comName;
+
+-- g) plant names that have the same hot zone as “Pink Caladiums” (your solution MUST get
+-- the hot zone of “Pink Caladiums” in a variable).
+
+-- h) the total number of plants, the minimum delivery size, the maximum delivery size, and
+-- the average size based on the plants that have delivery sizes (note that the average value
+-- should be rounded using two decimals).
+
+-- i) the Latin name of the plant that has the word ‘Eyed’ in its name (you must use LIKE in
+-- this query to get full credit).
+
+-- j) the exact output (ordered by Category and then by Name):
